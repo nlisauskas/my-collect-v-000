2e9d823afcac716058e36af1f4a72590827558d8
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-10 11:57:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
-def collect(collection)
+def my_collect(collection)
   i = 0
+  new_collection = []
   while i < collection.length
     yield(collection[i])
+    new_collection << collection[i]
     i = i + 1
   end
+  collection
 end
